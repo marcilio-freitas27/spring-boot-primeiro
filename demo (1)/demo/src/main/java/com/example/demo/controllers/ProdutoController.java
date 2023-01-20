19e159fe30dacb1f9499a10900f9b8a1f4949ed7
Marcilio Freitas <marcilio@beltsistemas.com.br>
2023-01-20 00:28:48 -0300

    Inserindo métodos delete(DeleteMappig, do tipo Optional<T>), put(PutMapping) e pesquisa por produto.

--- demo (1)/demo/src/main/java/com/example/demo/controllers/ProdutoController.java
+++ demo (1)/demo/src/main/java/com/example/demo/controllers/ProdutoController.java
@@ -3,8 +3,12 @@ package com.example.demo.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,6 +30,11 @@ public class ProdutoController {
         return new Produto("Café Santa Clara", 200.00, 0.15);
     }
 
+    @GetMapping("/{id}")
+    public Optional<Produto> obterProduto(@PathVariable int id){
+        return produtoRepository.findById(id);
+    }
+
     @GetMapping
     public Iterable<Produto> obterProdutos(){
         return produtoRepository.findAll();
@@ -41,6 +50,17 @@ public class ProdutoController {
         Produto produto = new Produto(nome, preco, desconto);
         produtoRepository.save(produto);
         return produto;
+    }
+
+    @PutMapping("/produtos")
+    public Produto alterarProduto(@Validated Produto produto){
+        produtoRepository.save(produto);
+        return produto;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable int id) {
+        produtoRepository.deleteById(id);
     }
 
 }
